7c5fa7c332b704d998afba39ebd576541cb29c87
Jarmo Hurri <jarmo.hurri@iki.fi>
2021-05-27 08:39:15 +0300

    * population initialization * main

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+  public static void main (String[] args)
+  {
+    TspGa ga = new TspGa (null, null, null, null, null);
+  }
+}
--- TspGa.java
+++ TspGa.java
@@ -1,10 +1,14 @@
-public class GeneticAlgorithm
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class TspGa
 {
-  public GeneticAlgorithm (Selection selection,
-                           Evaluation evaluation,
-                           Crossover crossover,
-                           Mutation mutation,
-                           Termination termination)
+  public TspGa (Selection selection,
+                Evaluation evaluation,
+                Crossover crossover,
+                Mutation mutation,
+                Termination termination)
   {
     this.selection = selection;
     this.evaluation = evaluation;
@@ -14,35 +18,48 @@ public class GeneticAlgorithm
 
     POPULATION_SIZE = 10;
     STARTCITY = 'X';
-    VISITED_CITIES; = "ABCDEFGHIJKLMNOPQRST".toCharArray ();
-    NUM_CITIES = CITIES.length;
+    VISITED_CITIES = "ABCDEFGHIJKLMNOPQRST".toCharArray ();
+    NUM_CITIES = VISITED_CITIES.length + 1;
     population = new char [POPULATION_SIZE][];
     initialize ();
+
+    for (char[] r : population)
+      System.out.println (java.util.Arrays.toString (r));
   }
   
-  Individual run ()
+  void run ()
   {
-    List<Integer> fitnesses = evaluation.evaluate (population);
+    // List<Integer> fitnesses = evaluation.evaluate (population);
 
-    boolean terminate = false;
-    while (!terminate)
-    {
-      population = selection.select (population, fitnesses);
-      population = crossover.crossover (population);
-      population = mutation.mutate (population);
-      List<Integer> newFitnesses = evaluation.evaluate (population);
-      if (Termination.terminate (newFitnesses, fitnesses))
-        terminate = true;
-      else
-        fitnesses = newFitnesses;
-    }
+    // boolean terminate = false;
+    // while (!terminate)
+    // {
+    //   population = selection.select (population, fitnesses);
+    //   population = crossover.crossover (population);
+    //   population = mutation.mutate (population);
+    //   List<Integer> newFitnesses = evaluation.evaluate (population);
+    //   if (Termination.terminate (newFitnesses, fitnesses))
+    //     terminate = true;
+    //   else
+    //     fitnesses = newFitnesses;
+    // }
   }
 
   private void initialize ()
   {
+    List<Character> visitedCities = new ArrayList<> ();
+    for (char c : VISITED_CITIES)
+      visitedCities.add (c);
+    
     for (int i = 0; i < POPULATION_SIZE; i++)
     {
-      
+      Collections.shuffle (visitedCities);
+      population [i] = new char [NUM_CITIES + 1];
+      population [i][0] = STARTCITY;
+      population [i][NUM_CITIES] = STARTCITY;
+      int j = 1;
+      for (char c : visitedCities)
+        population [i][j++] = c;
     }
   }
   
@@ -57,3 +74,9 @@ public class GeneticAlgorithm
   final char[] VISITED_CITIES;
   final int NUM_CITIES;
 }
+
+class Selection {}
+class Evaluation {}
+class Crossover {}
+class Mutation {}
+class Termination {}
