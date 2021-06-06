2e8c1b17784d5bdbd578bceaf78863e9c073739a
Jarmo Hurri <jarmo.hurri@iki.fi>
2021-06-06 12:36:15 +0300

    * license

--- TerminationRuleBestMid.java
+++ TerminationRuleBestMid.java
@@ -1,3 +1,18 @@
+// Copyright (C) 2021 Jarmo Hurri
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // terminates if no increase in fitness of best and no increase in
 // "median" fitness; assumes fitness arrays are in decreasing order
 public class TerminationRuleBestMid extends TerminationRule
